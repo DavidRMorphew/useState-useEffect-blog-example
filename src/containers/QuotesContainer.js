8092ef2fc4b4705ce1db75d0eb25607eebb09a76
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-10 11:50:27 -0400

    build skeleton of QuotesContainer imported into App

---
+++ src/containers/QuotesContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class QuotesContainer extends Component {
+
+    render(){
+        return(
+            <div>
+                <h1>Quotes Logic for Display Will Be in This Container Component</h1>
+            </div>
+        )
+    }
+}
+
+export default QuotesContainer
