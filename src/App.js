aba2e219866b3a988800f590ec8c48300bc27493
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-10 12:38:18 -0400

    add postsData and import into App; remove <body> tags

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import PostsContainer from './containers/PostsContainer'
 import PostsComponent from './components/PostsComponent'
+import postsData from './data/posts'
+console.log(postsData.posts)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Example App</h1>
-      </header>
-      <body>
+      </header>      
         <PostsContainer />
         <PostsComponent />
-      </body>
     </div>
   );
 }
---
+++ src/data/posts.js
@@ -0,0 +1,25 @@
+const postsData = {
+    name: "Posts Data",
+    posts: [
+        { 
+            id: 1, 
+            title: "My first post",
+            body: "I'd like to thank everyone for reading this first post.", 
+            likes: 0
+        },
+        { 
+            id: 2, 
+            title: "Why I post",
+            body: "I post because I'm special.", 
+            likes: 0
+        },
+        {
+            id: 3,
+            title: "My favorite quote",
+            body: `"You don't know what you're talking about" — Socrates`,
+            likes: 0
+        }
+    ]
+}
+
+export default postsData
