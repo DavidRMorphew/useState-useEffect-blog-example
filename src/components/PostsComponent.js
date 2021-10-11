5e90ce103d3d0ec2859dfa0213699e89bba04580
DavidRMorphew <60860458+DavidRMorphew@users.noreply.github.com>
2021-10-10 20:44:38 -0400

    add onClick to buttons & change displayPosts to variable name with value of posts mapping instead of funciton output

--- src/components/PostsComponent.js
+++ src/components/PostsComponent.js
@@ -6,34 +6,37 @@ import Post from './Post'
 const PostsComponent = (props) => {
 
     const [posts, setPosts] = useState(postsData.posts)
-    const [filterTerm, setFilterTerm] = useState("")
+    const [filterNum, setFilterNum] = useState(null)
     
-    const displayPosts = () => (
-        posts.map(post => <Post key={post.id} postInfo={post} updateLikes={updateLikes}/>)
-    )
-
+    
     const updateLikes = (id) => {
         const postToUpdate = posts.find(post => post.id === id)
         const updateIndex = posts.indexOf(postToUpdate)
         postToUpdate.likes += 1
         setPosts(prevPosts => [...prevPosts.slice(0, updateIndex), postToUpdate, ...prevPosts.slice(updateIndex + 1)])
     }
+    
+    const displayPosts = posts.map(post => <Post key={post.id} postInfo={post} updateLikes={updateLikes}/>)
+
+    const handleFilterNum = (num) => {
+        setFilterNum(num);
+    }
 
     const displayFilteredPosts = () => {
-
+        return posts.filter(post => post.likes >= filterNum)
     }
 
     return(
         <div>
             <h1>Posts Using Hooks Will Go Here</h1>
             <>      
-            <Button>0 Likes</Button>{' '}
-            <Button>5+ Likes</Button>{' '}
-            <Button>10+ Likes</Button>
+            <Button onClick={() => handleFilterNum(0)}>0 Likes</Button>{' '}
+            <Button onClick={() => handleFilterNum(5)}>5+ Likes</Button>{' '}
+            <Button onClick={() => handleFilterNum(10)}>10+ Likes</Button>
             </>
             <Container>
             <CardGroup>
-                {displayPosts()}
+                {displayPosts}
             </CardGroup>
             </Container>
         </div>
